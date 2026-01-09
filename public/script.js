44f1ff770e5557df13efd374d1423c95582da6f9
I.R. COMERCIO E MATERIAIS ELETRICOS LTDA <miguel.alves1@faculdadegran.edu.br>
2026-01-09 00:53:29 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -889,8 +889,8 @@ function showToast(message, type = 'success') {
     
     document.body.appendChild(messageDiv);
     
-    setTimeout(() => {
-        messageDiv.style.animation = 'slideOut 0.1s ease forwards';
-        setTimeout(() => messageDiv.remove(), 300);
-    }, 3000);
+   setTimeout(() => {
+    messageDiv.style.animation = 'slideOut 0.4s ease forwards';
+    setTimeout(() => messageDiv.remove(), 400);
+}, 600);
 }
